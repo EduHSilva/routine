6a6d91878db0031035180cc4adfc9f6ac82bccc8
Eduardo H <edu.slhenrique@gmail.com>
2024-12-30 21:57:49 -0300

    fixes

--- backend/main.go
+++ backend/main.go
@@ -1,25 +1,17 @@
-package handler
+package main
 
 import (
-	"fmt"
 	"github.com/EduHSilva/routine/config"
 	"github.com/EduHSilva/routine/router"
-	"net/http"
 )
 
 var (
 	logger *config.Logger
 )
 
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World!")
-}
-
-func Main() {
+func main() {
 	logger = config.GetLogger("main")
 	err := config.Init()
-
-	http.HandleFunc("/", Handler)
 
 	if err != nil {
 		logger.ErrF("Configuration initialization failed with error: %v", err)
